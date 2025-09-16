832a6f8b9f34902e44b5df2086f2d42d8f162cc6
Vitor Hugo Almeida <vitorhugo_as@hotmail.com>
2025-09-15 21:20:48 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import Alunos from "./pages/Alunos.jsx";
+import Professores from "./pages/Professores.jsx";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="alunos" element={<Alunos />} />
+        <Route path="professores" element={<Professores />} />
         {/* fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
