916833646e28e6b6369ee9fda8cb31f023d3f7ee
Vitor Hugo Almeida <vitorhugo_as@hotmail.com>
2025-09-15 21:19:29 -0300

    Add files via upload

---
+++ src/components/CrudProfessores/CrudProfessores.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import "./CrudProfessores.css";
+
+const API = "http://localhost:4000/api/professores";
+
+export default function CrudProfessores() {
+ 
+
+  return (
+    <h2>CRUD Professores</h2>
+  );
+}
